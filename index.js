6bd45e5220ebc3de9152cc2bcbd0c39f71737f5e
bredele <olivier.wietrich@gmail.com>
2013-12-03 00:25:37 -0700

    add first version, use event-plugin or other and classes component

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var events = require('delegate');
 
 /**
  * Expose 'Control'
@@ -11,16 +12,30 @@ module.exports = Control;
  * @api public
  */
 
-function Control() {
-  //do something
+function Control(scope) {
+	this.scope = scope;
+	this.current = null;
 }
 
 
-Control.prototype.toggle = function() {
-		
+Control.prototype.toggle = function(node, selector, type, callback, capture) {
+	var _this = this;
+	events.bind(node, selector, type, function(e){
+		e.target.classList.toggle('active');
+		_this.scope[callback].call(_this.scope, node);
+	}, (capture === true));
 };
 
 
-Control.prototype.radio = function() {
-	
+Control.prototype.radio = function(node, selector, type, callback, capture) {
+	var _this = this;
+	events.bind(node, selector, type, function(e){
+		//it seems there is a memory leak
+		var target = e.target;;
+		if(_this.current !== target && _this.current) _this.current.classList.remove('active');
+		target.classList.add('active');
+		_this.current = target;
+		_this.scope[callback].call(_this.scope, node);
+	}, (capture === true));
 };
+
